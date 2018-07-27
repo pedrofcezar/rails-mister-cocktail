7d27a7e2e424cb73e56bd5a6fe89f9d606de664c
Pedro Cezar <pedrofcezar@gmail.com>
2018-07-27 14:47:34 -0300

    adding delete

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -20,13 +20,14 @@ class CocktailsController < ApplicationController
     else
       render 'new'
     end
+  end
 
-    def destroy
+  def destroy
       @cocktail = Cocktail.find(params[:id])
       @cocktail.destroy
       redirect_to root_path
-    end
   end
+
 
   private
 
